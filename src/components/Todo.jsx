b0effb142788be27111de28e5f3a6a8319aca2fe
Artem Sergieiev <66088984+ArtemS83@users.noreply.github.com>
2021-11-21 15:54:55 +0100

    add checkbox

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   View,
   Text,
@@ -8,30 +8,58 @@ import {
   TouchableOpacity,
 } from 'react-native';
 
+import Checkbox from 'expo-checkbox';
+
 export const Todo = ({ todo, onRemoveTodo }) => {
+  const [isSelected, setSelection] = useState(false);
+
   const removeTodoHendler = () => onRemoveTodo(todo.id);
+
   return (
     <TouchableOpacity
+      style={styles.todoBox}
       onPress={() => {
-        console.log(todo.id);
+        // console.log(todo.id);
+        setSelection(!isSelected);
       }}
       //   onLongPress={() => onRemoveTodo(todo.id)}
       onLongPress={removeTodoHendler}
       activeOpacity={0.5}
     >
-      <Text style={styles.todo}>{todo.title}</Text>
+      <View style={styles.viewBox}>
+        <View style={styles.textBox}>
+          <Text style={styles.text}>{todo.title}</Text>
+        </View>
+        <Checkbox
+          value={isSelected}
+          // onValueChange={setSelection}
+          color={isSelected ? '#3949ab' : undefined}
+        />
+      </View>
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
-  todo: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    padding: 15,
+  todoBox: {
     marginBottom: 10,
     borderWidth: 1,
     borderColor: '#eee',
     borderRadius: 5,
+    backgroundColor: '#eee',
+  },
+  viewBox: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 15,
+  },
+
+  textBox: {
+    flex: 1,
+  },
+  text: {
+    marginRight: 10,
+    color: '#000',
   },
 });
